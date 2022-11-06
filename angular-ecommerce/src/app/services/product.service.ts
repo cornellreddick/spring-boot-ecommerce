219e152861db137a6abe9d597fd92fb404f7e914
Cornell Reddick <cornellreddick@outlook.com>
2022-11-06 17:29:33 -0500

    Added products to the UI.

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Product } from '../common/product';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -9,19 +9,19 @@ import { map } from 'rxjs/operators';
 })
 export class ProductService {
 
-private baseUrl = 'http://localhost:8080/api/products';
+  private baseUrl = 'http://localhost:8080/api/products';
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]>{
-  return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
-  map(response => response._embedded.products
-  );
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
   }
 }
 
 interface GetResponse {
-_embedded: {
-products: Product[];
-}
+  _embedded: {
+    products: Product[];
+  }
 }
