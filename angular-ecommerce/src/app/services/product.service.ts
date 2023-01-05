281552c72df76344e3dde19ed52b4e15697d8a1d
Cornell Reddick <cornellreddick@outlook.com>
2023-01-05 06:48:32 -0500

    Enhance menu component to read data from product service

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -11,6 +11,8 @@ export class ProductService {
 //Added size = 100 to temporary show all 100 products
   private baseUrl = 'http://localhost:8080/api/products';
 
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
@@ -18,14 +20,26 @@ export class ProductService {
  //need to build URL based on category id ... will come back to this!
  const searchURL = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
 
-    return this.httpClient.get<GetResponse>(searchURL).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchURL).pipe(
       map(response => response._embedded.products)
     );
   }
+
+  getProductCategories(): Observable<ProductCategory[]>{
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+          map(response => response._embedded.productCategory)
+        );
+  }
+
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
   }
 }
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
+  }
