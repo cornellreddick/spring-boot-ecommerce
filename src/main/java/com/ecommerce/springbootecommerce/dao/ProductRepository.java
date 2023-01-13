542d25e0ea7d8418a83d87c9aca2e78b3afe3698
Cornell Reddick <cornellreddick@outlook.com>
2023-01-13 10:47:05 -0500

    Updated ProductRepository to add search query and add a new search component

--- src/main/java/com/ecommerce/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/com/ecommerce/springbootecommerce/dao/ProductRepository.java
@@ -13,4 +13,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
 Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
+// SELECT * FROM Product p WHERE p.name LIKE CONCAT('%', :name , '%') example of how this query will look
+//add a new query method
+Page<Product> findByNameContaining(@Param("name") String name, Pageable page);
+
 }
