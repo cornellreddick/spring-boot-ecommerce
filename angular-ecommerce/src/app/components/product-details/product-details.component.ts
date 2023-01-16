a20d27c69a216757577334875144e09d9eca5f16
Cornell Reddick <cornellreddick@outlook.com>
2023-01-16 11:04:06 -0500

    Added method to handle product details.

--- angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+product!: Product;
+
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+  this.route.paramMap.subscribe(() +> {
+    this.handleProductDetails();
+  }
+
+  handleProductDetails(){
+    // get the "id" param string. convert string to number usinmg the "+" symbol
+   const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = product;
+        }
+      )
   }
 
 }
