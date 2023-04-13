f43acd989ac6390d2b4ec49b2e1d8832c34e70ab
Cornell Reddick <cornellreddick@outlook.com>
2023-04-13 09:11:40 -0400

    Added State Entity Class to display states in the Checkout page on the UI and mapped country to states

--- src/main/java/com/ecommerce/springbootecommerce/entity/Country.java
+++ src/main/java/com/ecommerce/springbootecommerce/entity/Country.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="country")
@@ -20,5 +21,6 @@ public class Country {
     @Column(name = "name")
     private String name;
 
-
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
 }
