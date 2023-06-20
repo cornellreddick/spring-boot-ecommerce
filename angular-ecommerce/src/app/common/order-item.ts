7c1dc7a6cb4299c5762dee00ede021e453e8692b
Cornell Reddick <cornellreddick@outlook.com>
2023-06-20 15:17:19 -0400

    Fixed issues with checkout service and checkout component.

--- angular-ecommerce/src/app/common/order-item.ts
+++ angular-ecommerce/src/app/common/order-item.ts
@@ -9,7 +9,7 @@ export class OrderItem {
       this.imageUrl = cartItem.imageUrl;
       this.quantity = cartItem.quantity;
       this.unitPrice = cartItem.unitPrice;
-      this.productId = cartItem.productId;
+      this.productId = cartItem.id;
   }
 }
 
--- angular-ecommerce/src/app/services/checkout.service.ts
+++ angular-ecommerce/src/app/services/checkout.service.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Purchase } from './purchase';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Purchase } from '../common/purchase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
 
-private purchaseUrl = 'https://localhost:8080';
+  private purchaseUrl = 'http://localhost:8080/api/checkout/purchase';
 
   constructor(private httpClient: HttpClient) { }
 
-  placeOrder(purchase: Purchase): Obeservable<any>{
+  placeOrder(purchase: Purchase): Observable<any> {
     return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
   }
+
 }
