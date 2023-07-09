bef7e7bf61e3110935c55ee845fe7774217f21ea
Cornell Reddick <cornellreddick@outlook.com>
2023-07-09 16:53:44 -0400

    Updated login component.

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+  oidc: {
+    clientId: 'il0szREcfh1toWFDtKU4fd9XOomCW4dO',
+    issuer: 'https://dev-onnyc5ata4y4uibf.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
